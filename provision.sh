e259aef9c2b3138cc62b45b9b8dec7b9834e1a7e
Daniel Einspanjer <deinspanjer@gmail.com>
2014-01-30 14:26:20 -0500

    Setup modulepath install java

--- provision.sh
+++ provision.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
-PUPPET_DIR=/etc/puppet/
-if [ ! -d "$PUPPET_DIR" ]; then
-  mkdir -p $PUPPET_DIR
-fi
+sudo rpm -ivh https://yum.puppetlabs.com/el/6/products/x86_64/puppetlabs-release-6-10.noarch.rpm
 
+yum makecache
+
+sed -i 's/enabled=1/&\npriority=1/' /etc/yum.repos.d/puppetlabs.repo
+
+yum update -y puppet
+
+puppet module install puppetlabs/java
+
+PUPPET_MODULE_PATH=$(puppet config print modulepath):$(pwd)/puppet/modules
+
+puppet apply --modulepath="$PUPPET_MODULE_PATH" puppet/manifests/default.pp
