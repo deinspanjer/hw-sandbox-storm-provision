a8e71acb9551df31c7e4d63a8b115da1cae8501f
Daniel Einspanjer <deinspanjer@gmail.com>
2014-01-30 13:33:24 -0500

    Okay, let's try without librarian-puppet.

--- provision.sh
+++ provision.sh
@@ -1,24 +1,7 @@
 #!/bin/sh
-# based on https://github.com/purple52/librarian-puppet-vagrant
 
-# Install rubygems but keep ruby at the existing version.
-yum install -y rubygems ruby-devel
-
-# Directory in which librarian-puppet should manage its modules directory
 PUPPET_DIR=/etc/puppet/
 if [ ! -d "$PUPPET_DIR" ]; then
   mkdir -p $PUPPET_DIR
 fi
-
-cp puppet/Puppetfile $PUPPET_DIR
-
-# Using patched version of librarian-puppet. See:
-# http://blog.csanchez.org/2013/01/24/managing-puppet-modules-with-librarian-puppet/
-if [ "$(gem search -i librarian-puppet)" = "false" ]; then
-  gem install librarian-puppet
-  cd $PUPPET_DIR && librarian-puppet install --clean
-else
-  cd $PUPPET_DIR && librarian-puppet update
-fi
-
 
