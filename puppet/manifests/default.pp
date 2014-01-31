4533ac9e77cc5eafa2ad3b2630ca62806e5d8a8b
Daniel Einspanjer <deinspanjer@gmail.com>
2014-01-31 15:58:44 -0500

    Is replace working?

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -2,6 +2,7 @@
 
 # A replace in file hack
 # Found: http://trac.cae.tntech.edu/infrastructure/browser/puppet/modules/common/manifests/defines/replace.pp?rev=169
+package { 'perl': }
 define replace($file, $pattern, $replacement) {
   $pattern_no_slashes = regsubst($pattern, '/', '\\/', 'G', 'U')
   $replacement_no_slashes = regsubst($replacement, '/', '\\/', 'G', 'U')
@@ -9,6 +10,7 @@ define replace($file, $pattern, $replacement) {
     command => "/usr/bin/perl -pi -e 's/${pattern_no_slashes}/${replacement_no_slashes}/' '${file}'",
     onlyif  => "/usr/bin/perl -ne 'BEGIN { \$ret = 1; } \$ret = 0 if /${pattern_no_slashes}/ && ! /\\Q${replacement_no_slashes}\\E/; END { exit \$ret; }' '${file}'",
     alias   => "exec_$name",
+    logoutput => true,
     require => Package["perl"],
   }
 }
